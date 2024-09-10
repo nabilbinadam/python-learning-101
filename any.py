54faf4f28870a3651acbf175d27849b3868c54bd
nabilbinadam <nabilbinadam@gmail.com>
2024-09-10 15:24:50 +0800

    learn any function

---
+++ any.py
@@ -0,0 +1,18 @@
+
+
+
+color = ["red","blue","green"]
+words = "sukablyat"
+pick = ["red"]
+#print(words.count("sukab"))
+
+"""if "sukja" in words:
+    print("got word")
+
+else:
+    print("not in words")   
+"""
+
+
+print(any(color in pick))
+
