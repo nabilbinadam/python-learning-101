1386babb551d82e3e0fe8086ef27b8f629d5f8f7
nabilbinadam <nabilbinadam@gmail.com>
2024-09-17 21:20:47 +0800

    practice

--- validcard.py
+++ validcard.py
@@ -14,61 +14,16 @@ The results of doubling are: 8569 6195 0383 3437
 Next, sum all the digits: 8+5+6+9+6+1+9+5+0+3+8+3+3+4+3+7 = 80
 If the sum is divisible by 10, then the credit card number is valid. Therefore, this number is valid!"""
 
-user_input=str(input("insert credit card:"))
 
+input = [int(input("Insert Credit Card :"))]
 
-def doubling(user_input):
-      
-    counter =[]
-    for doub in range(len(user_input)- 1, -1, -2):
-      #print(user_input[i])
-      print(doub)
-      ranging = doub*2
-      print (ranging)
-      
-      return ranging
-      
-    
-   
-    # sort + list 
-    total= ranging + list(user_input) 
-    total.sort()
-    counter.append(total)
-
-    print (counter)     
-
-
-doubling(user_input) 
-"""
-#check=checkDoublingGreaterThan9()
-def checkDoublingGreaterThan9(result):
-    sub=[]
-    for i in result :
-     if i < 9 :
-       subtract = i - 9
-       sub.append(subtract)
-       print (sub)
-    return sub
+def doubling(input):
+    for i in range(len(input)-1,len(input),-2) :
+       multi = input[i] * 2
+       return multi
 
 
 
 
 
-
-
-
-
-
-
-doubling(user_input)
-checkDoublingGreaterThan9(counter)  
-
-def sumDigit(result):
-   additional = 
-   return additional
-
-def Divisible(additional):
-   if additional % 10 ==0 :
-      print("Credit Card is Valid ") 
-
-"""
+print(doubling(input))  
