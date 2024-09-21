cc46f192488c84268cab2e62bbaf868819c950bc
nabilbinadam <nabilbinadam@gmail.com>
2024-09-21 19:57:41 +0800

    slice learning

---
+++ slice.py
@@ -0,0 +1,15 @@
+
+fruits = ['mango','apple','carrot','pear','cherry','grapes']
+
+fruit_dic= {
+   
+      'fruits':[['mango','apple','carrot','pear','cherry','grapes']],
+
+      'market': [['tesco','alamanda','ioi']],
+
+      'available': True ,
+
+}
+
+
+print(fruit_dic.fruits)
