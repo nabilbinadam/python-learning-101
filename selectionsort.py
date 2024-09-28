c31cef7780a0e122d368c42f23c5f6b6e2f01c4b
nabilbinadam <nabilbinadam@gmail.com>
2024-09-28 18:15:49 +0800

    selectionsort

--- selectionsort.py
+++ selectionsort.py
@@ -3,27 +3,28 @@ data= [6,5,2,5,8,3]
 
 
 
-#def selection(data):
+def selection(data):
+        length= len(data)
 
-length= len(data)
-for i in range(length):
-        min = data[i]
+        for i in range(length):
+            min = i # print element 6,5,2,5,8
+
+
         #print(min)
 
-        for j in range(min+1,1):
-                if data[j]< min:
-                        data[j],min = min,data[j]
+            for j in range(i+1,length): #index
 
-                        print(j)
+                #print (j)  # j kena generate [5,5,3]
+                
+                if data[j]<data[min]:
+
+                        min=j
+                        #print(j)
+                        data[i],data[min]=data[min],data[i]  # swap the element not the index.
+                        print(data)
+              
+                
 
 
-
-
-
-
-
-
-
-    
-
-#selection(data)
+#same number 
+print(selection(data))
