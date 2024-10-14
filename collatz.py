7e8333606e7c87b92a742f56fa1688b7bcd58b6c
nabilbinadam <nabilbinadam@gmail.com>
2024-10-14 22:44:21 +0800

    relearn the basic

---
+++ collatz.py
@@ -0,0 +1,14 @@
+
+n= int(input("Insert Integer:"))
+
+while n>1:
+    if n== 1:
+        print("not a valid number")
+
+    elif n %2 == 0:
+        n = n/2
+        print(n)
+
+    elif n % 2 !=0:
+        n = (3*n)+ 1
+        print(n)   
--- problem.py
+++ problem.py
@@ -1,4 +1,4 @@
-data= [1,2,3,4,5,6,7,8]
+data= [1,2,3,4,5,6,7,8,9]
 
 for i in data :
-    print("true")
+    print(i)
