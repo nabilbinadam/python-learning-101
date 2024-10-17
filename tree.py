14a6c470fca8d513e85bfa7e56611bdd0e2e2a91
nabilbinadam <nabilbinadam@gmail.com>
2024-10-17 21:36:24 +0800

    dictionary

---
+++ tree.py
@@ -0,0 +1,13 @@
+data = {
+    "name" : "nabil" ,
+    "adress": "no 6",
+    "ic": "043022",
+    "phone": "298448",
+    "user_type": "admin",
+
+}
+
+info = data.values()
+getting = data.items()
+print(info)
+print(getting)
