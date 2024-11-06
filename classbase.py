3bfa5a29a7a3fc26eba79ccd04ae187f259ae5ef
nabilbinadam <nabilbinadam@gmail.com>
2024-11-06 18:35:45 +0800

    classbase

---
+++ classbase.py
@@ -0,0 +1,22 @@
+
+class Car : 
+
+    def __init__(self,type,colour,plate):
+        
+        self.type=type
+        self.colour=colour
+        self.plate=plate
+
+
+    def showcase(colour) :
+
+        print(colour)
+
+        
+
+Car("sedan","red","blb582")
+
+Car.showcase("red")
+
+
+
