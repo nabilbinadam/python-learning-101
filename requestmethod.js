aab365117c2597d30dc32b967cbc91c8f640e449
nabilbinadam <nabilbinadam@gmail.com>
2024-11-24 15:26:10 +0800

    try

---
+++ requestmethod.js
@@ -0,0 +1,24 @@
+
+
+const request = ()=> {
+  url = "try"
+ try{
+      fetch(try)
+      .then( response=> response.json()
+
+
+      ).
+      then( if (!response.ok){console(success)}
+
+
+      )
+      .then( return json.data)
+
+
+ }
+ catch{}
+
+
+
+
+}
