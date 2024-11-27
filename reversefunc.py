5fde23026350f35033cec8edfdd7af044c87f53d
nabilbinadam <nabilbinadam@gmail.com>
2024-11-27 20:57:57 +0800

    practice

---
+++ reversefunc.py
@@ -0,0 +1,20 @@
+
+user_input=str(input(":"))
+
+
+def revers(user_input):
+
+
+    return user_input[::-1]
+    
+
+print(revers(user_input))
+
+n= int(input("insert N:"))
+def fiboseq(n):
+
+while n<
+   F=F(n-1)+F(n-2)
+   
+
+   return F
